2231355aadbe92f108c238358e14b320948875d2
Nickson Yap <hi@nickson.me>
2019-06-27 22:44:25 +0800

    Export almost done (not yet save file)

--- src/visualization/export_trajectories_side.m
+++ src/visualization/export_trajectories_side.m
@@ -5,17 +5,16 @@ load(fullfile(opts.dataset_path, 'ground_truth', 'trainval.mat'));
 trajectories = trainData;
 
 % Create folder
-folder = 'video-results';
+folder = 'export-results';
 mkdir([opts.experiment_root, filesep, opts.experiment_name, filesep, folder]);
-video_name = fullfile(opts.experiment_root, opts.experiment_name, folder, 'duke_sideview.mp4');
-
 
 % Params
-colors    = distinguishable_colors(1000);
-tail_size = 300;
-fps       = 120;
-rois      = opts.ROIs;
-ids       = unique(trajectories(:,2));
+colors     = distinguishable_colors(1000);
+% tail_size  = 300;
+tail_size  = 1;
+fps        = 120;
+rois       = opts.ROIs;
+ids        = unique(trajectories(:,2));
 interval   = opts.sequence_intervals{opts.sequence};
 startFrame = interval(1);
 endFrame   = interval(end);
@@ -29,67 +28,40 @@ end
 
 %% Render side view
 
-vid = VideoWriter(video_name, 'MPEG-4');
-open(vid);
-
 % Placeholders for tags
 inds = [271,1; 271, 481; 271, 961; 271,1441;  1,961; 1,1441; 1,481; 1,1];
 
 
 for frame = startFrame:fps:endFrame
-    fprintf('Frame %d/%d\n', frame, endFrame);
+    % fprintf('Frame %d/%d\n', frame, endFrame);
     
     data = trajectories(trajectories(:,3) == frame,:);
     ids = unique(data(:,2));
-    finalimg = zeros(540,1920,3);
     
     % Will read through each camera separately (slow)
     % TODO: Render each camera separately, then combine into mosaic (fast)
     for iCam = 1:opts.num_cam
-        
-        img = opts.reader.getFrame(iCam, global2local(opts.start_frames(iCam),frame));
+        % fprintf('Camera %d/%d\n', iCam, opts.num_cam);
         
         % Shade ROI with blue
-        roi = rois{iCam};
-        roimask = ones(size(img,1),size(img,2));
-        roimask = cv.fillPoly(roimask, roi);      
-        img = double(img);
-        c3 = img(:,:,3);
-        c3(logical(~roimask)) = 128 + 0.5*c3(logical(~roimask));
-        img(:,:,3) = c3;
-        img = uint8(img);
+        % roi = rois{iCam}; 
+        % disp(roi);
         
         % Draw all tails for current camera frame
         for k = 1:length(ids)
+        
             id = ids(k);
             mask = logical((trajectories(:,1) == iCam) .* (trajectories(:,2) == id) .* (trajectories(:,3) >= frame - tail_size) .* (trajectories(:,3) < frame));
-            bb= trajectories(mask, [4 5 6 7]);
-            feet = feetPosition(bb);
+            bb = trajectories(mask, [4 5 6 7]);
+            
             if ~isempty(bb)
-                img = insertObjectAnnotation(img,'rectangle', bb(end,:), id, 'Color', 255*colors(1+mod(id,1000),:));
+                % feet = feetPosition(bb);
+                % fprintf('origin: %d, %d, width: %d height: %d \n' , bb(:,1), bb(:,2), bb(:,3) , bb(:,4));
+                % fprintf('%06d-%02d: %05d - %04.1f, %04.1f \n' ,frame, iCam, k, feet(1,1), feet(1,2) );
+                fprintf('%06d-%02d: %05d - %04d, %04d - %04d, %04d \n' ,frame, iCam, k, bb(:,1), bb(:,2), bb(:,3), bb(:,4));
             end
             
-            img = cv.polylines(img, feet, 'Closed', false, 'Thickness', 4, 'Color', 255*colors(1+mod(id,1000),:));
-            
         end
-        img = imresize(img,0.25);
-        % Add image to mosaic
-        finalimg(inds(iCam,1):inds(iCam,1)+270-1,inds(iCam,2):inds(iCam,2)+480-1,:) = img;
     end
     
-    % Add camera tags
-    finalimg = uint8(finalimg);
-    finalimg = insertText(finalimg,[0 510], sprintf('Frame %d', frame),'FontSize',20);
-    finalimg = insertText(finalimg,[0 0], 'Camera 8', 'FontSize',20, 'BoxColor', 'black','BoxOpacity',0.8,'TextColor','white');
-    finalimg = insertText(finalimg,[480 0], 'Camera 7', 'FontSize',20, 'BoxColor', 'black','BoxOpacity',0.8,'TextColor','white');
-    finalimg = insertText(finalimg,[960 0], 'Camera 5', 'FontSize',20, 'BoxColor', 'black','BoxOpacity',0.8,'TextColor','white');
-    finalimg = insertText(finalimg,[1440 0], 'Camera 6', 'FontSize',20, 'BoxColor', 'black','BoxOpacity',0.8,'TextColor','white');
-    finalimg = insertText(finalimg,[0 270], 'Camera 1', 'FontSize',20, 'BoxColor', 'black','BoxOpacity',0.8,'TextColor','white');
-    finalimg = insertText(finalimg,[480 270], 'Camera 2', 'FontSize',20, 'BoxColor', 'black','BoxOpacity',0.8,'TextColor','white');
-    finalimg = insertText(finalimg,[960 270], 'Camera 3', 'FontSize',20, 'BoxColor', 'black','BoxOpacity',0.8,'TextColor','white');
-    finalimg = insertText(finalimg,[1440 270], 'Camera 4', 'FontSize',20, 'BoxColor', 'black','BoxOpacity',0.8,'TextColor','white');
-    
-    % Write mosaic frame to video
-    writeVideo(vid,finalimg);   
 end
-close(vid);
--- src/visualization/export_trajectories_top.m
+++ src/visualization/export_trajectories_top.m
@@ -8,13 +8,13 @@ trajectories = trainData;
 map = imread('src/visualization/data/map.jpg');
 
 % Create folder
-folder = 'video-results';
+folder = 'export-results';
 mkdir([opts.experiment_root, filesep, opts.experiment_name, filesep, folder]);
-video_name = fullfile(opts.experiment_root, opts.experiment_name, folder, 'duke_topview.mp4');
 
 % Params
 colors     = distinguishable_colors(1000);
-tail_size  = 300;
+% tail_size  = 300;
+tail_size  = 1;
 fps        = 120;
 rois       = opts.ROIs;
 ids        = unique(trajectories(:,2));
@@ -44,32 +44,22 @@ map(:,:,3) = map(:,:,3) .* roimask;
 map = uint8(map);
 
 %% Top View
-
-vid = VideoWriter(video_name, 'MPEG-4');
-open(vid);
 for frame = startFrame:fps:endFrame
-    fprintf('Frame %d/%d\n', frame, endFrame);
+    % fprintf('Frame %d/%d\n', frame, endFrame);
     
-    img = map;
     data = trajectories(trajectories(:,3) == frame,:);
     ids = unique(data(:,2));
     
-    polylines = [];
-    polycolors = [];
-    
     for k = 1:length(ids)
+        
         id = ids(k);
         mask = logical((trajectories(:,2) == id) .* (trajectories(:,3) >= frame - tail_size) .* (trajectories(:,3) < frame));
-        
         mapped = world2map(trajectories(mask, [8 9]));
-        polylines{k} = mapped;
-        polycolors{k} = colors(1+mod(id,1000),:);
-        img = cv.polylines(img, polylines{k}, 'Closed', false, 'Thickness', 4, 'Color', 255*colors(1+mod(id,1000),:));
+    
+        if(mapped) % weird case wheere mapped is blank...
+            fprintf('%06d: %05d - %07.2f, %07.2f \n' ,frame, k, mapped(1, 1), mapped(1, 2));
+        end
         
     end
-    img = rot90(img,3);
-    img = insertText(img,[0 0], sprintf('Frame %d', frame),'FontSize',20);
-    writeVideo(vid,img);
     
 end
-close(vid);
